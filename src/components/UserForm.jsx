3458035b476ddb69db06954b4807072ec4fa0bdb
kikedb <enriquediazbarrios@gmail.com>
2021-02-04 10:45:52 -0300

    Validando los campos del formulario

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,19 +1,51 @@
 import { Component } from 'react';
 
+const validate = values => {
+    const errors = {}
+    if(!values.name) {
+        errors.name = 'Este campo es obligatorio'
+    }
+    if(!values.email) {
+        errors.email = 'Este campo es obligatorio'
+    }
+    if(!values.website) {
+        errors.website = 'Este campo es obligatorio'
+    }
+    return errors
+}
 export default class UserForm extends Component {
+    state = {
+        errors: {}
+    }
+
     handleChange = ({ target }) => {
         this.setState({
             [target.value]: target.value
         })
     }
 
+    handleSubmit = e => {
+        e.preventDefault()
+        const {errors, ...sinErrors } = this.state
+        const result = validate(sinErrors)
+        this.setState({errors: result })
+        if(Object.keys(result).length) {
+            // Envio del formulario
+            e.target.reset()
+        }
+    }
+
     render() {
-        console.log(this.state);
+        const {errors} = this.state
         return (
-            <form>
-                <input onChange={this.handleChange} name="name" />
-                <input onChange={this.handleChange} name="email" />
-                <input onChange={this.handleChange} name="website" />
+            <form onSubmit={this.handleSubmit}>
+                <input placeholder="nombre" onChange={this.handleChange} name="name" />
+                {errors.name && <p>{errors.name}</p>}
+                <input placeholder="email" onChange={this.handleChange} name="email" />
+                {errors.email && <p>{errors.email}</p>}
+                <input placeholder="website" onChange={this.handleChange} name="website" />
+                {errors.website && <p>{errors.website}</p>}
+                <input type="submit" value="Enviar"/>
             </form>
         )
     }
