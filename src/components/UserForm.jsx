edae9412dc4a9f9c1c78d7638e2da43dc719b342
kikedb <enriquediazbarrios@gmail.com>
2021-02-04 10:30:39 -0300

    Construyendo la vista de formulario

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -1,9 +1,20 @@
 import { Component } from 'react';
 
 export default class UserForm extends Component {
+    handleChange = ({ target }) => {
+        this.setState({
+            [target.value]: target.value
+        })
+    }
+
     render() {
+        console.log(this.state);
         return (
-            'vista de formulario'
+            <form>
+                <input onChange={this.handleChange} name="name" />
+                <input onChange={this.handleChange} name="email" />
+                <input onChange={this.handleChange} name="website" />
+            </form>
         )
     }
 }
